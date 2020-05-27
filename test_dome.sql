1c3891226ace5b3b9ba62817b6edf59cf57fa8e0
Solly <mail4solly@gmail.com>
2020-05-27 15:57:14 +1000

    rename file

--- test_dome.sql
+++ test_dome.sql
@@ -1,12 +1,12 @@
 /*
 	Information about pets is kept in two separate tables:
-	TABLE dogs
-	  id INTEGER NOT NULL PRIMARY KEY,
-	  name VARCHAR(50) NOT NULL
+	CREATE TABLE dogs (
+	  id int NOT NULL PRIMARY KEY,
+	  name VARCHAR(50) NOT NULL)
 
-	TABLE cats
+	CREATE TABLE cats (
 		  id INTEGER NOT NULL PRIMARY KEY,
-		  name VARCHAR(50) NOT NULL
+		  name VARCHAR(50) NOT NULL)
 
 	Write a query that select all distinct pet names.
 */
@@ -20,15 +20,15 @@ select distinct name from
 
 /*
 	App usage data are kept in the following table:
-	TABLE sessions
+	CREATE TABLE sessions (
 	  id INTEGER PRIMARY KEY,
 	  userId INTEGER NOT NULL,
-	  duration DECIMAL NOT NULL
+	  duration DECIMAL NOT NULL)
 
 	  Write a query that selects userId and average session duration for each user who has more than one session.
 */
 
-select 
+select
 	userid, 
 	avg(duration) as avg
 from sessions 
@@ -43,10 +43,10 @@ group by
 	An employee is a manager if any other employee has their managerId set to the first employees id. 
 	An employee who is a manager may or may not also have a manager.
 
-	TABLE employees
-	  id INTEGER NOT NULL PRIMARY KEY
-	  managerId INTEGER REFERENCES employees(id)
-	  name VARCHAR(30) NOT NULL
+	CREATE TABLE employees(
+	  id INTEGER NOT NULL PRIMARY KEY,
+	  managerId INTEGER REFERENCES employees(id),
+	  name VARCHAR(30) NOT NULL)
 
 	  Write a query that selects the names of employees who are not managers.
 */
@@ -58,13 +58,13 @@ where id NOT IN (
 
 /*
 	The following two tables are used to define users and their respective roles:
-	TABLE users
+	CREATE TABLE users (
 	  id INTEGER NOT NULL PRIMARY KEY,
-	  userName VARCHAR(50) NOT NULL
+	  userName VARCHAR(50) NOT NULL)
 
-	TABLE roles
+	CREATE TABLE roles(
 	  id INTEGER NOT NULL PRIMARY KEY,
-	  role VARCHAR(20) NOT NULL
+	  role VARCHAR(20) NOT NULL)
 	  
 	The users_roles table should contain the mapping between each user and their roles. 
 	Each user can have many roles, and each role can have many users.
